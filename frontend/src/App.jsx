a894d196ca446be3a34ed3bd373061da53202bd4
hkakram97 <hkakram97@gmail.com>
2021-06-11 19:58:26 +0500

    remove send and verify components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,13 +12,13 @@ import SignUp from './components/signup'
 import Home from './components/Home'
 import UpdateProfile from './components/UpdateProfile'
 import ForgotPassword from './components/ForgotPassword'
-import SendEmailLink from './components/SendEmailLink'
-import VerifyEmailLink from './components/VerifyEmailLink'
+import SendVerifyLink from './components/SendVerifyLink'
 // context
 import { AuthProvider } from './context/AuthContext'
 
 // Route
 import PrivateRoute from './route/PrivateRoute'
+
 
 function App() {
 
@@ -27,7 +27,7 @@ function App() {
             <AuthProvider>
                 <Switch>
                     <Route path="/signin">
-                        <SignIn/>
+                        <SignIn />
                     </Route>
                     <Route path="/signup">
                         <SignUp />
@@ -35,19 +35,19 @@ function App() {
                     <Route path="/forgot-password">
                         <ForgotPassword />
                     </Route>
-                    
+
                     <PrivateRoute>
                         <Route exact path="/">
-                            <Home/>
+                            <Home />
                         </Route>
                         <Route path="/update-profile">
                             <UpdateProfile />
                         </Route>
                         <Route path="/send-email-link">
-                            <SendEmailLink/>
+                            <SendVerifyLink btnName={'Send Email Link'} status={true} />
                         </Route>
                         <Route path="/verify-email-link">
-                            <VerifyEmailLink/>
+                            <SendVerifyLink btnName={'Verify Email Link'} status={false} />
                         </Route>
                     </PrivateRoute>
                 </Switch>
